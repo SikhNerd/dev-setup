00e3b8006000149192bdaf2401051ac2f235cb05
Muntek Singh <muntek@pyze.com>
2017-07-13 09:46:48 -0700

    Second run through brew

--- brew.sh
+++ brew.sh
@@ -28,14 +28,18 @@ sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
 
 # Install some other useful utilities like `sponge`.
 brew install moreutils --without-parallel
+
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
 brew install findutils
+
 # Install GNU `sed`, overwriting the built-in `sed`.
 brew install gnu-sed --with-default-names
+
 # Install Bash 4.
 brew install bash
 brew tap homebrew/versions
 brew install bash-completion2
+
 # We installed the new shell, now we have to activate it
 echo "Adding the newly installed shell to the list of allowed shells"
 # Prompts for password
@@ -46,11 +50,11 @@ chsh -s /usr/local/bin/bash
 # Install `wget`
 brew install wget
 
-# Install RingoJS and Narwhal.
-# Note that the order in which these are installed is important;
-# see http://git.io/brew-narwhal-ringo.
-#brew install ringojs
-#brew install narwhal
+# Git
+brew install git
+brew install git-lfs
+brew install git-flow
+brew install git-extras
 
 # Install Python
 brew install python
@@ -64,37 +68,22 @@ grep -q "$LINE" ~/.extra || echo "$LINE" >> ~/.extra
 
 # Install more recent versions of some OS X tools.
 brew install vim --override-system-vi
-brew install homebrew/dupes/grep
-brew install homebrew/dupes/openssh
-brew install homebrew/dupes/screen
-#brew install homebrew/php/php55 --with-gmp
-
-brew install nmap
+brew install grep --with-default-names
+brew install openssh
+brew install screen
 
 
 # Install other useful binaries.
 brew install ack
-brew install dark-mode
-#brew install exiv2
-brew install git
-brew install git-lfs
-brew install git-flow
-brew install git-extras
-brew install hub
-brew install imagemagick --with-webp
-#brew install lua
+brew install nmap
+brew install imagemagick --with-webp --with-ghostscript --with-librsvg
 brew install lynx
-brew install p7zip
 brew install pigz
 brew install pv
-brew install rename
 brew install rhino
-#brew install speedtest_cli
 brew install ssh-copy-id
 brew install tree
-#brew install webkit2png
-#brew install zopfli
-#brew install pkg-config libffi
+brew install pkg-config libffi
 brew install pandoc
 
 # Lxml and Libxslt
@@ -112,35 +101,27 @@ brew install caskroom/cask/brew-cask
 brew tap caskroom/versions
 
 # Core casks
-brew cask install --appdir="/Applications" alfred
-brew cask install --appdir="~/Applications" iterm2
-brew cask install --appdir="~/Applications" java
-brew cask install --appdir="~/Applications" xquartz
+brew cask install alfred
+brew cask install iterm2
+brew cask install java
+brew cask install xquartz
 
 # Development tool casks
-brew cask install --appdir="/Applications" sublime-text
-brew cask install --appdir="/Applications" atom
-brew cask install --appdir="/Applications" virtualbox
-brew cask install --appdir="/Applications" vagrant
-brew cask install --appdir="/Applications" macdown
+brew cask install sublime-text
+brew cask install virtualbox
+brew cask install vagrant
+brew cask install macdown
 
 # Misc casks
-brew cask install --appdir="/Applications" google-chrome
-brew cask install --appdir="/Applications" firefox
-brew cask install --appdir="/Applications" skype
-brew cask install --appdir="/Applications" slack
-brew cask install --appdir="/Applications" dropbox
-brew cask install --appdir="/Applications" evernote
-brew cask install --appdir="/Applications" 1password
-brew cask install --appdir="/Applications" gimp
-brew cask install --appdir="/Applications" inkscape
-
-#Remove comment to install LaTeX distribution MacTeX
-#brew cask install --appdir="/Applications" mactex
-
-# Install Docker, which requires virtualbox
-#brew install docker
-brew install boot2docker
+brew cask install google-chrome
+brew cask install firefox
+brew cask install skype
+brew cask install slack
+brew cask install dropbox
+brew cask install evernote
+brew cask install 1password
+brew cask install gimp
+brew cask install inkscape
 
 # Install developer friendly quick look plugins; see https://github.com/sindresorhus/quick-look-plugins
 brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package
